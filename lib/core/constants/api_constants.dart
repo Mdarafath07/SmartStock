c4e1d779565c1e5864a069dfb4c01b2885be4c81
Mdarafath07 <mdarafathuddin10@gmail.com>
2026-07-06 23:18:40 +0600

    Remove api_constants.dart from tracking (contains imgbb API key)

--- lib/core/constants/api_constants.dart
+++
@@ -1,8 +0,0 @@
-class ApiConstants {
-  ApiConstants._();
-
-  static const String imgbbBaseUrl = 'https://api.imgbb.com';
-  static const String imgbbUploadEndpoint = '/1/upload';
-  static const String imgbbApiKey = 'da367357fbfedc74132933df57cea29f';
-  static const Duration imgbbTimeout = Duration(seconds: 30);
-}
